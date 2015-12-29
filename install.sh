2d0e4b9e5d7fc073c1cc4b7221452a9cf61a2aac
Mike King <yo@mikeking.io>
2015-12-28 20:25:21 -1000

    Update install script

--- install.sh
+++ install.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
+# ------------------------------------------------------- #
 # Setup Mac for web development
+# ------------------------------------------------------- #
+
 
 # ------------------------------------------------------- #
+# Utility functions
+# ------------------------------------------------------- #
+
+# Print result from last commands exit code
+
+function print_result(){
+
+  local result="$1"
+
+  local success="$2"
+
+  local failure="$3"
+
+	[ $result -eq 0 ] \
+	
+  && echo -e "$(tput setaf 148)$success$(tput sgr0)" \
+	
+  || echo -e "$(tput setaf 197)$failure$(tput sgr0)"
+}
+
+# ------------------------------------------------------- #
+# Installation functions
+# ------------------------------------------------------- #
+
 # Install Xcode Command Line Tools
 # https://github.com/paulirish/dotfiles/blob/master/setup-a-new-machine.sh
-# ------------------------------------------------------- #
 
-function install_xcode_tools() {
+function install_xcode_cli_tools() {
 
 	if ! xcode-select --print-path &> /dev/null; then
 
@@ -19,9 +45,7 @@ function install_xcode_tools() {
 	        sleep 5
 	    done
 
-		[ $1 -eq 0 ] \
-		&& echo -e "$(tput setaf 148) Installed Xcode Command Line Tools successfully. $(tput sgr0)" \
-		|| echo -e "$(tput setaf 197) There was an error. Xcode command line tools not installed $(tput sgr0)"
+      print_result "$?" "Installed Xcode Command Line Tools successfully." "There was an error. Xcode command line tools not installed"
 
 	    # Point the `xcode-select` developer directory to
 	    # the appropriate directory from within `Xcode.app`
@@ -29,22 +53,21 @@ function install_xcode_tools() {
 
 	    sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
 
-		[ $1 -eq 0 ] \
-		&& echo -e "$(tput setaf 148) Made `xcode-select` developer directory point to Xcode. $(tput sgr0)" \
-		|| echo -e "$(tput setaf 197) Unable to point `xcode-select` developer directory to Xcode. $(tput sgr0)"
+      print_result "$?" "Made `xcode-select` developer directory point to Xcode." "Unable to point `xcode-select` developer directory to Xcode."
 
 	    # Prompt user to agree to the terms of the Xcode license
 	    # https://github.com/alrra/dotfiles/issues/10
 
 	    sudo xcodebuild -license
 
-		[ $1 -eq 0 ] && echo -e "$(tput setaf 148) Agreed with the Xcode Command Line Tools license. $(tput sgr0)"
+      print_result "$?" "Agreed with the Xcode Command Line Tools license."
 
 	fi
 
 }
 
 # Run setup
+
 echo "   _____     __         ______     __  __     ______        ______     ______     ______ "
 echo " /\  __ \   /\ \       /\  __ \   /\ \_\ \   /\  __ \      /\  == \   /\  == \   /\  __ \ "
 echo " \ \  __ \  \ \ \____  \ \ \/\ \  \ \  __ \  \ \  __ \     \ \  __<   \ \  __<   \ \ \/\ \ "
@@ -53,8 +76,16 @@ echo "   \/_/\/_/   \/_____/   \/_____/   \/_/\/_/   \/_/\/_/      \/_____/   \/
 
 echo -e "\n";
 
-echo -e "Let's get your machine setup!\n";
+echo -e "Welcome to @micjamking's computer setup. Let's #GSD!\n";
 
-echo -e "First things first, we need to install Xcode command line tools...\n" && install_xcode_tools;
-echo -e "Brewing all the things...\n" && source install/brew.sh;
-echo -e "Installing the only real programming language (node)...\n" && source install/node.sh;
+echo -e "First things first, we need to install Xcode command line tools...\n"
+
+install_xcode_cli_tools
+
+echo -e "Brewing all the things...\n"
+
+source install/brew.sh
+
+echo -e "Installing the only real programming language (node)...\n"
+
+source install/node.sh
