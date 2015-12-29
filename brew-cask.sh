6b6e200e3660f71c8b1910635df3dc37215bf153
Mike King <yo@mikeking.io>
2015-12-28 14:49:40 -1000

    Updating brew-cask with applications

--- brew-cask.sh
+++ brew-cask.sh
@@ -10,15 +10,23 @@
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
-# dev
-brew cask install iterm2
-
-# fun
-brew cask install miro-video-converter
-
-# browsers
+# Browsers
+brew cask install firefox
+brew cask install google-chrome
 brew cask install google-chrome-canary
-brew cask install firefox-nightly
-brew cask install webkit-nightly
-brew cask install chromium
-brew cask install torbrowser
+
+# Development
+brew cask install iterm2
+brew cask install android-studio
+brew cask install atom
+brew cask install dash
+brew cask install github-desktop
+brew cask install iterm2
+brew cask install kaleidoscope
+brew cask install sketch
+
+# Other
+brew cask install miro-video-converter
+brew cask install google-drive
+brew cask install music-manager
+brew cask install gopro-studio
