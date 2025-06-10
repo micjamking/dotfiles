9fdf05e82f129f8513bab0d49c01657f5da489ad
Mike King <micjamking@users.noreply.github.com>
2025-06-10 01:12:07 -1000

    Update node.sh

--- install/node.sh
+++ install/node.sh
@@ -9,16 +9,11 @@ nvm alias default stable
 nvm use default
 
 echo "Install npm packages...\n";
-npm install -g bower
-npm install -g doctoc
-npm install -g grunt-cli
-npm install -g grunt-init
 npm install -g http-server
 npm install -g jsdoc
 npm install -g markdown-preview
-npm install -g node-sass
+npm install -g sass
 npm install -g npm-check-updates
 npm install -g postcss
-npm install -g yo
 
 echo "NPM package installation complete. Exiting the Node...\n";
