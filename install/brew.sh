906313a5b30297a7d95542060fc3a6adda65603e
Mike King <micjamking@users.noreply.github.com>
2025-06-10 00:34:59 -1000

    Update brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -3,7 +3,7 @@
 # Install command-line tools using Homebrew.
 
 echo "Install Homebrew\n";
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Ask for the administrator password upfront.
 sudo -v
@@ -15,7 +15,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+brew upgrade
 
 echo "Install GNU core utilities (those that come with OS X are outdated).\n";
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -24,38 +24,30 @@ brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 echo "Install GNU sed, overwriting the built-in sed.\n";
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 
 echo "Install Bash 4.\n";
 brew install bash
 
-echo "Install Homebrew versions.\n";
-brew tap homebrew/versions
-
 echo "Install autocompletion.\n";
 brew install bash-completion
-brew tap homebrew/completions
-brew install wpcli-completion
 
 echo "Install more recent versions of some OS X tools.\n";
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
+brew install vim
+brew install grep
+brew install openssh
+brew install screen
 
 echo "Install other useful binaries.\n";
 brew install git
 brew install ffmpeg
-brew install heroku-toolbelt
-brew install imagemagick --with-webp
+brew install imagemagick
 brew install mongodb
 brew install postgresql
 brew install mysql
 brew install nvm
 brew install trash
 brew install tree
-brew install homebrew/php/wp-cli
 
 echo "Remove outdated versions from the cellar.\n";
 brew cleanup
